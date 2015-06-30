29d96a0f6810c932f726b298585757305fcec255
fullflu <k.takayama0902@gmail.com>
2015-06-30 20:44:54 +0900

    Update camera2.py

--- camera2.py
+++ camera2.py
@@ -33,6 +33,7 @@ def skinget(im_hsv):
     cv2.imshow("skin",im_skin)
     return mask_skin
 
+
 i=0
 
 while True:
@@ -64,4 +65,55 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-
+def convex_defects(mode,cnt,centr,im_skin):
+# get convexhull and convexdefects (usually we don't use this)
+    if mode == 2:
+        try:
+            hull = cv2.convexHull(cnt,returnPoints = False)
+        except:
+            mode=0
+            print("hull")
+            #break
+            
+        try:
+            defects = cv2.convexityDefects(cnt,hull)
+        except:
+            mode=0
+            print("defects")
+            #break
+        
+        if defects ==None:
+            mode=0
+            #break
+        
+        a0 = [0,0]
+        min_cos = 0
+        yubi = (0,0)
+        for i in range(defects.shape[0]):
+            s,e,f,d = defects[i,0]
+            start = tuple(cnt[s][0])
+            end = tuple(cnt[e][0])
+            far = tuple(cnt[f][0])
+            #dist = cv2.pointPolygonTest(cnt,centr,True)
+            
+            #visualize hand outline by green line
+            cv2.line(im_skin,start,end,[0,255,0],2) 
+            #visualize hand corner by red circle
+            cv2.circle(im_skin,far,5,[0,0,255],-1)
+            
+            a1 = [start[0]-end[0],start[1]-end[1]]
+            #get cosine
+            cos = (a1[0]*a0[0]+a1[1]*a0[0])/(sqrt(a1[0]**2+a1[1]**2)+sqrt(a0[0]**2+a0[1]**2))
+            if cos < min_cos:
+                min_cos = cos
+                yubi = start
+            a0 = a1
+        
+        #visualize the tip of finger circle
+        #cv2.circle(im_skin,yubi,5,[255,0,0],2)
+        
+        #direction to which finger direct
+        vec = (yubi[0]-centr[0], yubi[1]-centr[1])
+    return vec
+        #visualize the vec as blue line
+        #cv2.line(im_skin,centr,yubi,[255,0,0],2)
