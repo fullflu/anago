93d8a86dfd1588175d3e6497611700cbdb53c89a
fullflu <k.takayama0902@gmail.com>
2015-07-04 22:38:47 +0900

    Update py_servo.ino

--- py_servo.ino
+++ py_servo.ino
@@ -5,14 +5,13 @@ int servo_count = 3;
 Servo servo[3];
 
 typedef struct ServoWeight {
-  float forward; // 動かすサーボのうち前サーボの重み
-  float back; // 動かすサーボのうち，後ろサーボの重み
+  double forward_by_back;//前方のservo[(i + 1) % servo_count] を後方のservo[i] に対してどの強さで引くか
 } ServoWeight;
 
 // 使うサーボが1個の時の重み
 ServoWeight weight[3];
 // 使うサーボが2個の時の重み
-ServoWeight weight2[3];
+//ServoWeight weight2[3];
 
 //now skip the definition of the value of servo_weight
 
@@ -25,6 +24,9 @@ void setup() {
   servo[1].write(0);
   servo[2].attach(3);
   servo[2].write(0);
+  weight[0].forward_by_back = 1.1; 
+  weight[1].forward_by_back = 0.95; 
+  weight[2].forward_by_back = 0.9;
   delay(2000);
 }
 
@@ -52,31 +54,29 @@ void loop() {
     stan = i; 
     change = (i + 1) % servo_count;
     change_deg_weight = sin(deg_rad) / sin(2 * PI / 3.0 - deg_rad);
-    //change_servo_weight = ,,,;
+    change_servo_weight = weight[i].forward_by_back;
   }
   else{
     stan = (i + 1) % servo_count;
     change = i;
     change_deg_weight = sin(2 * PI / 3.0 - deg_rad) / sin(deg_rad);
-    //change_servo_weight = ,,,;
+    change_servo_weight = 1.0 / weight[i].forward_by_back;
   }
   for(pos = 0; pos <= 180; pos += 1){
     //adjust pos by degree
-    /* when you use servo_weight, write like this
+    //when you use servo_weight, write like this
     if(change_servo_weight > 1.0) pos /= change_servo_weight;
     servo[change].write(int(pos * change_deg_weight * change_servo_weight));
-    */
-    servo[change].write(int(pos * change_deg_weight));
+    //servo[change].write(int(pos * change_deg_weight));
     servo[stan].write(pos);
     delay(delay_duration);
   }
   for(pos = 180; pos >= 0; pos -= 1){
     //adjust pos by degree
-    /* when you use servo_weight, write like this
+    //when you use servo_weight, write like this
     if(change_servo_weight > 1.0) pos /= change_servo_weight;
     servo[change].write(int(pos * change_deg_weight * change_servo_weight));
-    */
-    servo[change].write(int(pos * change_deg_weight));
+    //servo[change].write(int(pos * change_deg_weight));
     servo[stan].write(pos);
     delay(delay_duration);
   }
